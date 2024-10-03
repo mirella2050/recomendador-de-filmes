4f2c2bc55010de5a4bf1c8edc268379287f5c6b8
mirella2050 <mirella.stakoviaki@escola.pr.gov.br>
2024-10-03 11:49:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+// a viagem de chihiro, LIVRE, fantasia, aventura 
+// guardiões da galáxia, 12, fantasia, aventura 
+// as aventuras de pi, 10, drama, fantasia, aventura 
+// paddington, LIVRE, fantasia, aventura 
+// ladrões de bicicleta, 12, drama 
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
