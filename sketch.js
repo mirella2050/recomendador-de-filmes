a438fc6f18a2615d1c60e0e718a5f506e94e5e14
mirella2050 <mirella.stakoviaki@escola.pr.gov.br>
2024-10-17 12:00:03 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,25 @@
-// a viagem de chihiro, LIVRE, fantasia, aventura 
-// guardiões da galáxia, 12, fantasia, aventura 
-// as aventuras de pi, 10, drama, fantasia, aventura 
-// paddington, LIVRE, fantasia, aventura 
-// ladrões de bicicleta, 12, drama 
+//cruella 12,comedia, aventura
+//procurando nemo, livre, aventura, comedia
+//a freira,14,terror, misterio 
+//enola holmes, 12 , misterio 
+//meu malvado favorito 4,10, comedia
+//a noiva cadaver livre, fantasia
+//coraline e o mundo secreto ,10, terror , fantasia
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-}
+} let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}  
