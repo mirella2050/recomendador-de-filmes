b9721463a1e6782760d8e2ea5b1c91bc64ae8cfb
mirella2050 <mirella.stakoviaki@escola.pr.gov.br>
2024-10-31 08:49:58 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,64 @@
-//cruella 12,comedia, aventura
-//procurando nemo, livre, aventura, comedia
-//a freira,14,terror, misterio 
-//enola holmes, 12 , misterio 
-//meu malvado favorito 4,10, comedia
-//a noiva cadaver livre, fantasia
-//coraline e o mundo secreto ,10, terror , fantasia
+//homem aranha,12,ação
+//branquelas,15,comédia
+//jumanji,13,comedia,ação
+//a milhões de quilometro,12,drama
+//a teia,16,mistério
+//freira,14,terror
+//noiva cadaver,12,suspense
 function setup() {
   createCanvas(400, 400);
 }
-
-function draw() {
-  background(220);
-} let campoIdade;
-let campoFantasia;
-let campoAventura;
+let campoOrcamento;
+let campoPraia;
+let campoCidadeGrande;
 
 function setup() {
   createCanvas(800, 400);
   createElement("h2", "Recomendador de filmes");
-  createSpan("Sua idade:");
-  campoIdade = createInput("5");
-  campoFantasia = createCheckbox("Gosta de fantasia?");
-  campoAventura = createCheckbox("Gosta de aventura?");
-}  
+  createSpan("idade");
+  campoOrcamento = createInput("12");
+  campoPraia = createCheckbox("Gosta de fantasia?");
+  campoCidadeGrande = createCheckbox("Gosta de comedia e misterio?");
+}
+
+function draw() {
+  background("rgb(228,24,24)");
+  let orcamento = parseFloat(campoOrcamento.value());
+  let gostaDePraia = campoPraia.checked();
+  let gostaDeCidadesGrandes = campoCidadeGrande.checked();
+  let recomendacao = geraRecomendacao(orcamento, gostaDePraia, gostaDeCidadesGrandes);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(orcamento, gostaDePraia, gostaDeCidadesGrandes) {
+  if (orcamento >= 1000) {
+    if (gostaDePraia) {
+      return "homem aranha.";
+    } else if (gostaDeCidadesGrandes) {
+      return "branquelas.";
+    } else {
+      return "a freira.";
+    }
+  } else if (orcamento >= 500) {
+    if (gostaDePraia) {
+      return "jumanji.";
+    } else if (gostaDeCidadesGrandes) {
+      return "a milhões de quilometros.";
+    } else {
+      return "a noiva cadaver.";
+    }
+  } else {
+    if (gostaDePraia) {
+      return "a teia.";
+    } else if (gostaDeCidadesGrandes) {
+      return "";
+    } else {
+      return "";
+    }
+  }
+}
+
